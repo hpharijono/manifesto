d1ab1e54c8ec0f797c54bee0a1237402d8e9c31f
Harris Harijono <harris@ingenuity.ph>
2020-08-07 02:32:20 +0800

    Added admin

--- apps/manifesto/admin.py
+++ apps/manifesto/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Value, Principle
+
+class ValueAdmin(admin.ModelAdmin):
+    pass
+
+
+class PrincipleAdmin(admin.ModelAdmin):
+    pass
+
 
 # Register your models here.
+admin.site.register(Value, ValueAdmin)
+admin.site.register(Principle, PrincipleAdmin)
