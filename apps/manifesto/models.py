05c719e7865a25d80dd38a98f7633d138a6e539a
Harris Harijono <harris@ingenuity.ph>
2020-08-07 02:32:38 +0800

    Added str for django-admin display

--- apps/manifesto/models.py
+++ apps/manifesto/models.py
@@ -5,8 +5,14 @@ class Value(models.Model):
     value = models.CharField(max_length=250)
     description = models.TextField()
 
+    def __str__(self):
+        return self.value
+
 
 class Principle(models.Model):
     principle = models.CharField(max_length=250)
     description = models.TextField()
+
+    def __str__(self):
+        return self.principle
 
